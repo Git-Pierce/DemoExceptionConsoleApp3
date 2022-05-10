09fdea9b1c46fa3bc9e53b032333b5339f88742a
Git-Pierce <shuitsing@pierce.ctc.edu>
2022-05-09 18:36:47 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Hello, World!");
+        PrintSomething();
+        TryCatchMethod();
+
+    }
+
+    private static void TryCatchMethod()
+    {
+        try
+        {
+            string numString = "10";
+            int num = Int32.Parse(numString);  //converting string to integer
+            Console.WriteLine($"Contents of num: {num}");
+        }
+        catch (Exception error) // -most common catch version
+        {
+
+            Console.WriteLine(error.Message);
+        }
+        //catch(FormatException) //version 2 format your own error msg
+        //{
+        //    Console.WriteLine("Error: num needs a valid value!");
+
+        //}
+    }
+
+    static void PrintSomething()
+    {
+        Console.WriteLine("Print Something!");
+    }
+}
